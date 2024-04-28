64e39acb3fa1f741ea561e4ec0a595102d8b12cb
smidovav <115158131+smidovav@users.noreply.github.com>
2024-04-28 15:45:48 +0200

    Create KvizovaOdpoved.java

---
+++ projekt-pc2t/bin/smidovaprojekt4/KvizovaOdpoved.java
@@ -0,0 +1,18 @@
+package smidovaprojekt4;
+
+/**
+ *
+ * @author smido
+ */
+public class KvizovaOdpoved {
+    public String odpoved;
+    public boolean jeSpravne;
+    
+    public void vypis() {
+        if (jeSpravne) {
+            System.out.println("+" + odpoved);
+        } else {
+            System.out.println("-" + odpoved);
+        }
+    }
+}
