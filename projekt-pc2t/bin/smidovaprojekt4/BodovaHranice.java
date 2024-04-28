6cb51414623b58fdb15d296d2cad60bb5a1684c3
smidovav <115158131+smidovav@users.noreply.github.com>
2024-04-28 15:30:18 +0200

    Create BodovaHranice.java

---
+++ projekt-pc2t/bin/smidovaprojekt4/BodovaHranice.java
@@ -0,0 +1,15 @@
+package smidovaprojekt4;
+
+/**
+ *
+ * @author smido
+ */
+public class BodovaHranice {
+    public String hodnoceni;
+    public int hranice;
+    
+    public void vypis(){
+    System.out.println("!" + hranice + "%" + hodnoceni);
+    }
+    
+}
