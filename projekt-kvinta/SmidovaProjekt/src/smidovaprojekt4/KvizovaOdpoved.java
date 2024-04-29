e5c673aeaecd1fd000f5029fa98c30db138f327f
smidovav <115158131+smidovav@users.noreply.github.com>
2024-04-29 20:52:55 +0200

    Update KvizovaOdpoved.java

--- projekt-kvinta/SmidovaProjekt/src/smidovaprojekt4/KvizovaOdpoved.java
+++ projekt-kvinta/SmidovaProjekt/src/smidovaprojekt4/KvizovaOdpoved.java
@@ -1,19 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package smidovaprojekt4;
 
-/**
- *
- * @author smido
- */
 public class KvizovaOdpoved {
-    public String odpoved; // uchovava odpoved
-    public boolean jeSpravne; // urcuje, jestli je odpoved spravna
+    public String odpoved;
+    public boolean jeSpravne;
     
-    public void vypis() { // vypise odpoved
+    public void vypis() {
         if (jeSpravne) {
             System.out.println("+" + odpoved);
         } else {
