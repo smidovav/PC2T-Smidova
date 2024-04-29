28e9e57fc43d20b9dfde311ad5b0fb5d3192c291
smidovav <115158131+smidovav@users.noreply.github.com>
2024-04-29 20:53:40 +0200

    Update KvizovaOtazka.java

--- projekt-kvinta/SmidovaProjekt/src/smidovaprojekt4/KvizovaOtazka.java
+++ projekt-kvinta/SmidovaProjekt/src/smidovaprojekt4/KvizovaOtazka.java
@@ -1,23 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package smidovaprojekt4;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author smido
- */
 public class KvizovaOtazka {
-    public String otazka; // nazev otazky
-    public ArrayList<KvizovaOdpoved> odpovedi; // kolekce odpovedi
-    public int body; // kolik bodu je za danou otazku dle vstupniho souboru
-    public boolean jeTextova = false; // rozhoduje, jestli je otazka otevrena nebo uzavrena
+    public String otazka;
+    public ArrayList<KvizovaOdpoved> odpovedi;
+    public int body;
+    public boolean jeTextova = false;
     
-    public void vypis() { // vypise otazku tak, jak je ve vstupnim souboru
+    public void vypis() {
         if (jeTextova) {
             System.out.println("?=" + otazka);
         } else {
@@ -29,7 +21,7 @@ public class KvizovaOtazka {
         }
     }
     
-    public String[] odpovediArray(){ // prizpusobeni pro GUI
+    public String[] odpovediArray(){
         String arr[] = new String[odpovedi.size()];
         for (int i = 0; i < odpovedi.size(); i++) {
             arr[i] = odpovedi.get(i).odpoved;
